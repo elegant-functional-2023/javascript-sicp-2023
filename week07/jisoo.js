7bb6d6817183d4a6152d2b1082006eb632750d39
이지수 <91363333+thgr8ganzi@users.noreply.github.com>
2023-11-04 11:31:30 +0900

    Update jisoo.js

--- week07/jisoo.js
+++ week07/jisoo.js
@@ -4,18 +4,5 @@ const isZero = (x) => applyGeneric('isZero', x);
 put('isZero', 'number', (x) => numer(x) === 0);
 put('isZero', 'rational', (x) => rational(x) === 0);
 put('isZero', 'complex', (x) => complex(x) === 0);
-// 2.87
-const isZeroPoly = (poly) => {
-    const areTermsZero = (termList) => {
-        if (isEmptyTermList(termList)) {
-            return true;
-        } else {
-            return isZero(coeff(firstTerm(termList))) &&
-                areTermsZero(restTerms(termList));
-        }
-    };
-    return areTermsZero(termList(poly));
-};
-const isZeroPoly2 = (p) => isEmptyTermList(termList(p));
-put('isZero', 'polynomial', isZeroPoly);
+
 
