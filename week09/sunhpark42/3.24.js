92f1822bcfaa0a4a260da449118553f087b94bcd
sunnypark <sunhpark42@gmail.com>
2023-11-25 11:55:35 +0900

    3.24 solution 추가

--- week09/sunhpark42/3.24.js
+++ week09/sunhpark42/3.24.js
@@ -20,25 +20,25 @@ function make_table(same_key) {
   const local_table = list("*table*");
 
   function lookup(key_1, key_2) {
-    const subtable = assoc(key_1, tail(local_table));
+    const subtable = assoc(key_1, tail(local_table), same_key);
 
     if (is_undefined(subtable)) {
       return undefined;
     } else {
-      const record = assoc(key_2, tail(subtable));
+      const record = assoc(key_2, tail(subtable), same_key);
       return is_undefined(record) ? undefined : tail(record);
     }
   }
 
   function insert(key_1, key_2, value) {
-    const subtable = assoc(key_1, tail(local_table));
+    const subtable = assoc(key_1, tail(local_table), same_key);
     if (is_undefined(subtable)) {
       set_tail(
         local_table,
         pair(list(key_1, pair(key_2, value)), tail(local_table))
       );
     } else {
-      const record = assoc(key_2, tail(subtable));
+      const record = assoc(key_2, tail(subtable), same_key);
       if (is_undefined(record)) {
         set_tail(subtable, pair(pair(key_2, value), tail(subtable)));
       } else {
